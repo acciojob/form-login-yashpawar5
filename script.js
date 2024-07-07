5d880d35acf46d9069b8835a87a3f71699dd09a0
Yash Pawar <89527379+yashpawar5@users.noreply.github.com>
2024-07-07 11:09:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementsByName("fname")[0].value;
+	const lastName = document.getElementsByName("lname")[0].value;
+	const fullName = firstName + " " + lastName;
 
+	alert(fullName);
 }
